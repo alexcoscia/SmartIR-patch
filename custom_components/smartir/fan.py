38ba1465f80df5e71656a13f7adc6fafa5de85b2
alexcoscia <101573852+alexcoscia@users.noreply.github.com>
2025-11-20 22:42:18 +0100

    Update fan.py

--- custom_components/smartir/fan.py
+++ custom_components/smartir/fan.py
@@ -8,17 +8,20 @@ import voluptuous as vol
 
 from homeassistant.components.fan import (
     FanEntity, FanEntityFeature,
-    PLATFORM_SCHEMA, DIRECTION_REVERSE, DIRECTION_FORWARD)
+    PLATFORM_SCHEMA, DIRECTION_REVERSE, DIRECTION_FORWARD
+)
 from homeassistant.const import (
-    CONF_NAME, STATE_OFF, STATE_ON, STATE_UNKNOWN)
+    CONF_NAME, STATE_OFF, STATE_ON, STATE_UNKNOWN
+)
 from homeassistant.core import Event, EventStateChangedData, callback
-from homeassistant.helpers.event import async_track_state_change, async_track_state_change_event
+from homeassistant.helpers.event import async_track_state_change_event
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.util.percentage import (
     ordered_list_item_to_percentage,
     percentage_to_ordered_list_item
 )
+
 from . import COMPONENT_ABS_DIR, Helper
 from .controller import get_controller
 
@@ -44,6 +47,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_POWER_SENSOR): cv.entity_id
 })
 
+
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the IR Fan platform."""
     device_code = config.get(CONF_DEVICE_CODE)
@@ -57,20 +61,25 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     device_json_path = os.path.join(device_files_absdir, device_json_filename)
 
     if not os.path.exists(device_json_path):
-        _LOGGER.warning("Couldn't find the device Json file. The component will " \
-                        "try to download it from the GitHub repo.")
+        _LOGGER.warning(
+            "Couldn't find the device Json file. The component will "
+            "try to download it from the GitHub repo."
+        )
 
         try:
-            codes_source = ("https://raw.githubusercontent.com/"
-                            "smartHomeHub/SmartIR/master/"
-                            "codes/fan/{}.json")
+            codes_source = (
+                "https://raw.githubusercontent.com/"
+                "smartHomeHub/SmartIR/master/"
+                "codes/fan/{}.json"
+            )
 
             await Helper.downloader(codes_source.format(device_code), device_json_path)
         except Exception:
-            _LOGGER.error("There was an error while downloading the device Json file. " \
-                          "Please check your internet connection or if the device code " \
-                          "exists on GitHub. If the problem still exists please " \
-                          "place the file manually in the proper directory.")
+            _LOGGER.error(
+                "Error while downloading the device Json file. "
+                "Check your internet connection or if the device code exists. "
+                "Otherwise place the file manually."
+            )
             return
 
     try:
@@ -83,9 +92,8 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         _LOGGER.error("The device JSON file is invalid")
         return
 
-    async_add_entities([SmartIRFan(
-        hass, config, device_data
-    )])
+    async_add_entities([SmartIRFan(hass, config, device_data)])
+
 
 class SmartIRFan(FanEntity, RestoreEntity):
     def __init__(self, hass, config, device_data):
@@ -103,179 +111,166 @@ class SmartIRFan(FanEntity, RestoreEntity):
         self._commands_encoding = device_data['commandsEncoding']
         self._speed_list = device_data['speed']
         self._commands = device_data['commands']
-        
+
         self._speed = SPEED_OFF
         self._direction = None
         self._last_on_speed = None
         self._oscillating = None
-        self._support_flags = (
+
+        # NEW HOME ASSISTANT FEATURE FLAG SYSTEM
+        self._attr_supported_features = (
             FanEntityFeature.SET_SPEED
             | FanEntityFeature.TURN_OFF
-            | FanEntityFeature.TURN_ON)
+            | FanEntityFeature.TURN_ON
+        )
 
-        if (DIRECTION_REVERSE in self._commands and \
-            DIRECTION_FORWARD in self._commands):
+        if (
+            DIRECTION_REVERSE in self._commands
+            and DIRECTION_FORWARD in self._commands
+        ):
             self._direction = DIRECTION_REVERSE
-            self._support_flags = (
-                self._support_flags | FanEntityFeature.DIRECTION)
-        if ('oscillate' in self._commands):
-            self._oscillating = False
-            self._support_flags = (
-                self._support_flags | FanEntityFeature.OSCILLATE)
+            self._attr_supported_features |= FanEntityFeature.DIRECTION
 
+        if "oscillate" in self._commands:
+            self._oscillating = False
+            self._attr_supported_features |= FanEntityFeature.OSCILLATE
 
         self._temp_lock = asyncio.Lock()
         self._on_by_remote = False
 
-        #Init the IR/RF controller
+        # Init IR/RF controller
         self._controller = get_controller(
             self.hass,
-            self._supported_controller, 
+            self._supported_controller,
             self._commands_encoding,
             self._controller_data,
-            self._delay)
+            self._delay,
+        )
 
     async def async_added_to_hass(self):
-        """Run when entity about to be added."""
         await super().async_added_to_hass()
-    
+
         last_state = await self.async_get_last_state()
 
         if last_state is not None:
-            if 'speed' in last_state.attributes:
-                self._speed = last_state.attributes['speed']
+            if "speed" in last_state.attributes:
+                self._speed = last_state.attributes["speed"]
 
-            #If _direction has a value the direction controls appears 
-            #in UI even if SUPPORT_DIRECTION is not provided in the flags
-            if ('direction' in last_state.attributes and \
-                self._support_flags & FanEntityFeature.DIRECTION):
-                self._direction = last_state.attributes['direction']
+            if (
+                "direction" in last_state.attributes
+                and self._attr_supported_features & FanEntityFeature.DIRECTION
+            ):
+                self._direction = last_state.attributes["direction"]
 
-            if 'last_on_speed' in last_state.attributes:
-                self._last_on_speed = last_state.attributes['last_on_speed']
+            if "last_on_speed" in last_state.attributes:
+                self._last_on_speed = last_state.attributes["last_on_speed"]
 
             if self._power_sensor:
-                async_track_state_change_event(self.hass, self._power_sensor, 
-                                               self._async_power_sensor_changed)
+                async_track_state_change_event(
+                    self.hass,
+                    self._power_sensor,
+                    self._async_power_sensor_changed,
+                )
 
     @property
     def unique_id(self):
-        """Return a unique ID."""
         return self._unique_id
 
     @property
     def name(self):
-        """Return the display name of the fan."""
         return self._name
 
     @property
     def state(self):
-        """Return the current state."""
-        if (self._on_by_remote or \
-            self._speed != SPEED_OFF):
+        if (self._on_by_remote or self._speed != SPEED_OFF):
             return STATE_ON
         return SPEED_OFF
 
     @property
     def percentage(self):
-        """Return speed percentage of the fan."""
-        if (self._speed == SPEED_OFF):
+        if self._speed == SPEED_OFF:
             return 0
 
         return ordered_list_item_to_percentage(self._speed_list, self._speed)
 
     @property
     def speed_count(self):
-        """Return the number of speeds the fan supports."""
         return len(self._speed_list)
 
     @property
     def oscillating(self):
-        """Return the oscillation state."""
         return self._oscillating
 
     @property
     def current_direction(self):
-        """Return the direction state."""
         return self._direction
 
     @property
     def last_on_speed(self):
-        """Return the last non-idle speed."""
         return self._last_on_speed
 
     @property
-    def supported_features(self):
-        """Return the list of supported features."""
-        return self._support_flags
-
-    @property
     def extra_state_attributes(self):
-        """Platform specific attributes."""
         return {
-            'last_on_speed': self._last_on_speed,
-            'device_code': self._device_code,
-            'manufacturer': self._manufacturer,
-            'supported_models': self._supported_models,
-            'supported_controller': self._supported_controller,
-            'commands_encoding': self._commands_encoding,
+            "last_on_speed": self._last_on_speed,
+            "device_code": self._device_code,
+            "manufacturer": self._manufacturer,
+            "supported_models": self._supported_models,
+            "supported_controller": self._supported_controller,
+            "commands_encoding": self._commands_encoding,
         }
 
     async def async_set_percentage(self, percentage: int):
-        """Set the desired speed for the fan."""
-        if (percentage == 0):
-             self._speed = SPEED_OFF
+        if percentage == 0:
+            self._speed = SPEED_OFF
         else:
             self._speed = percentage_to_ordered_list_item(
-                self._speed_list, percentage)
+                self._speed_list, percentage
+            )
 
-        if not self._speed == SPEED_OFF:
+        if self._speed != SPEED_OFF:
             self._last_on_speed = self._speed
 
         await self.send_command()
         self.async_write_ha_state()
 
-    async def async_oscillate(self, oscillating: bool) -> None:
-        """Set oscillation of the fan."""
+    async def async_oscillate(self, oscillating: bool):
         self._oscillating = oscillating
-
         await self.send_command()
         self.async_write_ha_state()
 
     async def async_set_direction(self, direction: str):
-        """Set the direction of the fan"""
         self._direction = direction
 
-        if not self._speed.lower() == SPEED_OFF:
+        if self._speed != SPEED_OFF:
             await self.send_command()
 
         self.async_write_ha_state()
 
-    async def async_turn_on(self, percentage: int = None, preset_mode: str = None, **kwargs):
-        """Turn on the fan."""
+    async def async_turn_on(self, percentage=None, preset_mode=None, **kwargs):
         if percentage is None:
             percentage = ordered_list_item_to_percentage(
-                self._speed_list, self._last_on_speed or self._speed_list[0])
+                self._speed_list, self._last_on_speed or self._speed_list[0]
+            )
 
         await self.async_set_percentage(percentage)
 
     async def async_turn_off(self):
-        """Turn off the fan."""
         await self.async_set_percentage(0)
 
     async def send_command(self):
         async with self._temp_lock:
             self._on_by_remote = False
             speed = self._speed
-            direction = self._direction or 'default'
+            direction = self._direction or "default"
             oscillating = self._oscillating
 
-            if speed.lower() == SPEED_OFF:
-                command = self._commands['off']
+            if speed == SPEED_OFF:
+                command = self._commands["off"]
             elif oscillating:
-                command = self._commands['oscillate']
+                command = self._commands["oscillate"]
             else:
-                command = self._commands[direction][speed] 
+                command = self._commands[direction][speed]
 
             try:
                 await self._controller.send(command)
@@ -283,8 +278,7 @@ class SmartIRFan(FanEntity, RestoreEntity):
                 _LOGGER.exception(e)
 
     @callback
-    async def _async_power_sensor_changed(self, event: Event[EventStateChangedData]) -> None:
-        """Handle power sensor changes."""
+    async def _async_power_sensor_changed(self, event: Event[EventStateChangedData]):
         entity_id = event.data["entity_id"]
         old_state = event.data["old_state"]
         new_state = event.data["new_state"]
